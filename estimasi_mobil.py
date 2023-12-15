8656b74206291a4598c6017cc7de60c0a9e34912
taliyameyswara <taliyameyswara@gmail.com>
2023-12-16 04:10:40 +0700

    first commit

---
+++ estimasi_mobil.py
@@ -0,0 +1,23 @@
+import pickle
+import streamlit as st
+
+model = pickle.load(open('estimasi_mobil.sav', 'rb'))
+
+# Judul web
+st.title('Estimasi Harga Mobil Bekas')
+
+# Form Inputan
+year = st.number_input("Input Tahun Mobil")
+mileage = st.number_input("Input Km Mobil")
+tax = st.number_input("Input Pajak Mobil")
+mpg = st.number_input('Input Konsumsi BBM Mobil')
+engineSize = st.number_input("Input Engine Size")
+
+predict = ''
+
+if st.button('Estimasi Harga'):
+    predict = model.predict(
+        [[year, mileage, tax, mpg, engineSize]]
+    )
+    st.write('Estimasi harga mobil bekas dalam Ponds : ', predict)
+    
